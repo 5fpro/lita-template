5fcada84be670c4d4f1679803ed5e932c7fa2600
marsz <marsz330@gmail.com>
2015-12-05 15:50:09 +0800

    command route & rspec

--- lita-handler-example/lib/lita/handlers/handler_example.rb
+++ lita-handler-example/lib/lita/handlers/handler_example.rb
@@ -2,6 +2,15 @@ module Lita
   module Handlers
     class HandlerExample < Handler
       # insert handler code here
+      config :auto_save, type: :boolean
+
+      route(/^echo\s+(.+)/, :echo, command: true, help: { "echo TEXT" => "help desc..." })
+      def echo(response)
+        response.reply(response.matches[0])
+        response.reply(response.args.inspect)
+        response.reply(response.match_data.inspect)
+        response.reply(response.user.name)
+      end
 
       Lita.register_handler(self)
     end
--- lita-handler-example/spec/lita/handlers/handler_example_spec.rb
+++ lita-handler-example/spec/lita/handlers/handler_example_spec.rb
@@ -1,4 +1,9 @@
 require "spec_helper"
 
 describe Lita::Handlers::HandlerExample, lita_handler: true do
+  it "#echo" do
+    send_command "echo 123123"
+    expect(replies.size).to be > 0
+    expect(replies.first).to match("123123")
+  end
 end
